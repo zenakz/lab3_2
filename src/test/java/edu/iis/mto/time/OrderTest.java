395ba956dd37c6bd248224404f49afe37283ee8f
Michal <michalogloza97@gmail.com>
2020-05-08 16:35:59 +0200

    Added ConfirmAndSubmitAtSameTimeTest

--- src/test/java/edu/iis/mto/time/OrderTest.java
+++ src/test/java/edu/iis/mto/time/OrderTest.java
@@ -1,16 +1,43 @@
 package edu.iis.mto.time;
 
+import org.joda.time.DateTime;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class OrderTest
 {
+    private static DateTime time;
+    private static SystemClock systemClock;
+    private static FakeClock fakeClock;
+
+    @BeforeAll
+    static void init()
+    {
+        time = new DateTime();
+        systemClock = new SystemClock();
+        fakeClock = new FakeClock();
+    }
+
     @Test
     void ConfirmOrderJustAfterSubmitTest()
     {
-        Order order = new Order(new SystemClock());
+        Order order = new Order(systemClock);
         order.addItem(new OrderItem());
+
+        order.submit();
+        assertDoesNotThrow(order::confirm);
+    }
+
+    @Test
+    void ConfirmAndSubmitAtSameTimeTest()
+    {
+        Order order = new Order(fakeClock);
+        order.addItem(new OrderItem());
+
+        fakeClock.setTime(time);
         order.submit();
         assertDoesNotThrow(order::confirm);
     }
