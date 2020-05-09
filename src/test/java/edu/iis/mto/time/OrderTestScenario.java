dbf91cc134fa4c011e158cc9df3e0ed5e9e131c9
Kamil Chrustowski <chrustek123456789@gmail.com>
2020-05-09 23:54:01 +0200

    Implemented successfully the first test case of scenario

--- src/test/java/edu/iis/mto/time/OrderTestScenario.java
+++ src/test/java/edu/iis/mto/time/OrderTestScenario.java
@@ -1,25 +1,44 @@
 package edu.iis.mto.time;
 
+import org.joda.time.DateTime;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
+import static org.hamcrest.Matchers.equalTo;
 import static org.junit.jupiter.api.Assertions.*;
-
+import static org.hamcrest.MatcherAssert.assertThat;
 class OrderTestScenario {
 
     private Clock mockedClock;
-
+    private Order order;
+    private DateTime testDateTime;
+    private OrderItem item;
 
     @BeforeEach
     void setUp() {
         mockedClock = Mockito.mock(Clock.class);
+        order = new Order();
+        testDateTime = DateTime.now();
+        item = Mockito.mock(OrderItem.class);
+        order.setSystemClock(mockedClock);
+        order.addItem(item);
     }
 
     @Test
     void orderConfirmationRequestedMoreThan24hoursAfterOrderSubmissionShouldEndUpWithOrderExpiredExceptionAndCancelledStatus() {
+        Mockito.doReturn(testDateTime)
+                .doReturn(testDateTime.plusHours(25))
+                .when(mockedClock)
+                .getCurrentDateTime();
 
+        order.submit();
+
+        assertThrows(OrderExpiredException.class, () -> order.confirm());
+
+        var result = order.getOrderState();
+        assertThat(result, equalTo(Order.State.CANCELLED));
     }
 
     @Test
