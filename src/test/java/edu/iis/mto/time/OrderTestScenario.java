a27a91ad1da576e3b10cc847a22e2c8eaab9c4cd
Kamil Chrustowski <chrustek123456789@gmail.com>
2020-05-09 21:57:55 +0200

    Generated basic OrderTestScenario class

---
+++ src/test/java/edu/iis/mto/time/OrderTestScenario.java
@@ -0,0 +1,17 @@
+package edu.iis.mto.time;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class OrderTestScenario {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+}
