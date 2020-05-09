6ca973e1d47ccc697f8c2858dcc6b7779df93aee
Kamil Chrustowski <chrustek123456789@gmail.com>
2020-05-09 23:04:57 +0200

    Added two test cases declarations and mocking Clock interface

--- src/test/java/edu/iis/mto/time/OrderTestScenario.java
+++ src/test/java/edu/iis/mto/time/OrderTestScenario.java
@@ -2,14 +2,32 @@ package edu.iis.mto.time;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class OrderTestScenario {
 
+    private Clock mockedClock;
+
+
     @BeforeEach
     void setUp() {
+        mockedClock = Mockito.mock(Clock.class);
     }
+
+    @Test
+    void orderConfirmationRequestedMoreThan24hoursAfterOrderSubmissionShouldEndUpWithOrderExpiredExceptionAndCancelledStatus(){
+
+    }
+
+    @Test
+    void orderConfirmationRequestedLessThan24HoursAfterOrderSubmissionShouldNotEndUpWithOrderExpiredExceptionAndCancelledStatus(){
+
+    }
+
+    
 
     @AfterEach
     void tearDown() {
